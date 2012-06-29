b71628150d20b2c5480e4843ee8e8b056b199de4
Kenny Younger <kyounger@gmail.com>
2012-06-29 16:28:55 -0500

    remove a bunch of stuff I don't use; update various configs; add tmux

--- hg/hgext.symlink/hg-pbranch/tut/buildall.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-./build.sh pbranch/config pbranch/simple pbranch/email pbranch/export pbranch/insert pbranch/dist pbranch/graph pbranch/pullupstream pbranch/collab pbranch/octopus pbranch/sanity
-
