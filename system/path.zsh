77a4afac11d12e79358ed86725796e5a7b30a98e
Kenneth Younger <kenny@kretina.(none)>
2012-10-05 23:10:08 -0500

    various changes

--- system/path.zsh
+++ system/path.zsh
@@ -1,6 +1,6 @@
-export PATH="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin:/opt/local/bin:/opt/local/sbin:$PATH"
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
 
-export MANPATH="/opt/local/Library/Frameworks/Python.framework/Versions/2.7/share/man:/opt/local/share/man:$MANPATH"
+export MANPATH="/opt/local/share/man:$MANPATH"
 
 if [[ -d "$HOME/bin" ]] ; then
     PATH="$HOME/bin:$PATH"
