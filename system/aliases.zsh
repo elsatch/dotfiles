b35e4f418f5b783a1a9a56d6301dd66bed91a649
Kenny Younger <kyounger@gmail.com>
2012-10-06 01:51:25 -0500

    a bit of cleanup

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -117,6 +117,15 @@ alias reload!='. ~/.zshrc'
 
 function ytmp3() { youtube-dl -kvt --extract-audio --audio-quality 160k --audio-format mp3 $1 }
 
+
+# Remove the hosts that I don't want to keep around- in this case, only
+# keep the first host. Like a boss.
+alias hosts="head -2 ~/.ssh/known_hosts | tail -1 > ~/.ssh/known_hosts"
+
+# Pipe my public key to my clipboard. Fuck you, pay me.
+alias pubkey="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+
 function mlregex() {
     if [[ "$#" != "1" ]]; then
         return
--- system/path.zsh
+++
@@ -1,7 +0,0 @@
-export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
-
-export MANPATH="/opt/local/share/man:$MANPATH"
-
-if [[ -d "$HOME/bin" ]] ; then
-    PATH="$HOME/bin:$PATH"
-fi
