314f7d64c18484f6a23da5a337aa2e1093ed3017
Kenny Younger <kyounger@gmail.com>
2012-10-19 13:35:57 -0500

    cleanup aliases file

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,25 +1,39 @@
 export CLICOLOR=1
 
-# macports update to all ports except pcre (pcre 8.30 breaks WP)
+# temp alias: macports update to all ports except pcre (pcre 8.30 breaks WP)
 alias macports_upgrade_cmd="sudo port upgrade `port list outdated | awk '{print $1}' | grep -v pcre | tr '\n' ' '`"
 
-alias revkitch='rsync -r ~/projects/revolutionkitchen/revkitch-theme/* kenny@web2:/www/wordpress/wp-content/themes/rk-bootstrap/'
-
-alias xcode='open -a xcode'
+# open in...
 alias pre='open -a Preview'
+alias mvim='mvim --remote-tab-silent'
+alias firefox='/Applications/Firefox.app/Contents/MacOS/firefox-bin -profilemanager'
+
+# list files
 alias l='ls -al'
+alias lh='ls -ald .*'
+alias lsd3='du -sk * | sort -nr | head -3'
+
+# change dir (or make and change)
 alias u='cd ..'
 alias uu='cd ..; cd ..'
 alias uuu='cd ..; cd ..; cd ..'
+function mkcd () { mkdir -p "$@" && cd "$@"; }
+
+# other misc
 alias ttop='top -U $USER'
-alias lsd3='du -sk * | sort -nr | head -3'
-alias mvim='mvim --remote-tab-silent'
+alias cwd="pwd | tr -d '\n' | pbcopy"
 alias wgetssl='wget --no-check-certificate'
 alias gst='git status'
+alias grep='grep --color=auto --exclude=\.svn'
+alias secureemptytrash='srm -rs /Users/kenny/.Trash/*'
+alias rmdotsvn='find . -name ".svn" -type d -exec rm -rf {} \;'
+alias linecount="sed '/^\s*$/d' | wc -l"
 
+# where does a 301 point to
+function expandurl() { wget -q -U Mozilla/5 -O/dev/null -S $@ 2>&1 | awk '{ if($1~/^Location:/) { print $2 } }'; }
 
-function ta()
-{
+# this is how I start and reattach tmux
+function ta() {
     # use this to find out what to set TMPDIR to if ta() wasn't used:
     # `echo /var/folders/**/*/-Tmp-(U/)`
     # then attache with `TMPDIR=$$ tmux attach`
@@ -33,18 +47,19 @@ function ta()
     fi
 }
 
-function flushdns()
-{
+# flush osx dns (including dnsmasq)
+function flushdns() {
     dscacheutil -flushcache
     sudo kill -HUP `cat /opt/local/var/run/dnsmasq.pid`
     # sudo port unload dnsmasq
     # sudo port load dnsmasq
 }
 
+# get image height/width and copy to clipboard
 function gih () { identify -format "%h" "$@" | tr -d '\n' | pbcopy; }
 function giw () { identify -format "%w" "$@" | tr -d '\n' | pbcopy; }
-function mkcd () { mkdir -p "$@" && cd "$@"; }
 
+# try to extract any archive
 function extract () {
     if [ -f $1 ] ; then
         case $1 in
@@ -89,43 +104,28 @@ alias mysql_restart='mysql_stop; mysql_start'
 # apache
 alias restart_apache='sudo /opt/local/apache2/bin/apachectl restart'
 
-alias lh='ls -algH --color=always | egrep --color=never "\..*$"'
-alias cwd="pwd | tr -d '\n' | pbcopy"
-alias lh='ls -ald .*'
-alias grep='grep --color=auto --exclude=\.svn'
-alias secureemptytrash='srm -rs /Users/kenny/.Trash/*'
-alias rmdotsvn='find . -name ".svn" -type d -exec rm -rf {} \;'
-alias firefox='/Applications/Firefox.app/Contents/MacOS/firefox-bin -profilemanager'
-
-#function mirror_page_old() { wget --ignore-length --mirror --page-requisites --convert-links --no-verbose --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:6.0) Gecko/20100101 Firefox/6.0" --directory-prefix=./ $1 }
-
-# function mirror_page() { wget --adjust-extension --page-requisites --span-hosts --backup-converted --ignore-length --convert-links --no-verbose --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:6.0) Gecko/20100101 Firefox/6.0" --directory-prefix=./ $1 }
+# # wget mirrors a page for local archiving
+# function mirror_page() { 
+#     wget --adjust-extension --page-requisites --span-hosts --backup-converted --ignore-length --convert-links --no-verbose --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:6.0) Gecko/20100101 Firefox/6.0" --directory-prefix=./ $1 
+# }
  
-alias linecount="sed '/^\s*$/d' | wc -l"
-
-function expandurl() { curl -sIL $1 | grep ^Location; }
-
-function mygrants()
-{
+# show my grants in mysql
+function mygrants() {
   mysql -B -N $@ -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR ''', user, '''@''', host, ''';') AS query FROM mysql.user" | mysql $@ | sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}'
 }
 
-# brought here from dotfiles/zsh/aliases.zsh before it was deleted
-alias dotf='cd $ZSH'
-alias findps='ps aux | grep -v grep | grep'
-alias reload!='. ~/.zshrc'
+# snag an mp3 from youtube and title it properly
+function ytmp3() {
+    youtube-dl -kvt --extract-audio --audio-quality 160k --audio-format mp3 $1
+}
 
-function ytmp3() { youtube-dl -kvt --extract-audio --audio-quality 160k --audio-format mp3 $1 }
-
-
-# Remove the hosts that I don't want to keep around- in this case, only
-# keep the first host. Like a boss.
+# Remove the hosts that I don't want to keep around - in this case, only keep the first host.
 alias hosts="head -2 ~/.ssh/known_hosts | tail -1 > ~/.ssh/known_hosts"
 
-# Pipe my public key to my clipboard. Fuck you, pay me.
-alias pubkey="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"
+# Pipe my public key to my clipboard.
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
-
+# helper function to build multiline regex with '+' line continuation character (as/400 CL)
 function mlregex() {
     if [[ "$#" != "1" ]]; then
         return
@@ -138,27 +138,23 @@ function mlregex() {
     joiner='\\+\\s*\\n?^\\s*'
 
     for ((i = 1; i < $len; i++));
-        do
-            if [[ $i -ne 1 ]]; then
-                final="$final|"
-            fi
+    do
+        if [[ $i -ne 1 ]]; then
+            final="$final|"
+        fi
+        final="${final}${x:0:$i}${joiner}${x:$i:$len}"
+    done
 
-            final="${final}${x:0:$i}${joiner}${x:$i:$len}"
-        done
-
-        echo -n "$final|$x"
+    echo -n "$final|$x"
 
     # return $final
 }
 
-function expand-url() { 
-    wget -q -U Mozilla/5 -O/dev/null -S $@ 2>&1 | awk '{ if($1~/^Location:/) { print $2 } }';
-}
-
+# base64 a file and copy to clipboard
 function base64 () { openssl base64 < "$1" | tr -d '\n' | pbcopy }
 
-function cdf ()
-{
+# change directory to whatever the most recently focused finder window is
+function cdf () {
   CURRFOLDERPATH=$( /usr/bin/osascript <<"    EOT"
     tell application "Finder"
       try
